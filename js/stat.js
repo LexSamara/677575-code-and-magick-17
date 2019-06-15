ac59623de5edfa510f259260aa8d5435f3a0b8d4
LexSamara <eryomin.aa@gmail.com>
2019-06-15 20:50:55 +0300

    Исправлены замечания

--- js/stat.js
+++ js/stat.js
@@ -16,10 +16,10 @@ var COLUMN_ORIGIN_OFFSET_X = 30;
 var COLUMN_ORIGIN_OFFSET_Y = 80;
 
 var NAMES_OFFSET_X = 30;
-var NAMES_OFFSET_Y = -10;
+var NAMES_OFFSET_Y = -20;
 var NAMES_DELTA_X = 90;
 
-var TEXT_UNDER_COLUMN_GAP = 5;
+var TEXT_UNDER_COLUMN_GAP = 15;
 
 var titleTextArray = ['Ура, вы победили!', 'Список результатов:'];
 
@@ -30,47 +30,54 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 // Вывод текстового массива на экран
-var fillTextArr = function (ctx, arr, x, y, delta, column) {
+var renderTaxt = function (ctx, arr, x, y, column) {
   ctx.textBaseline = 'hanging';
   var yCoord = y;
   var xCoord = x;
   for (var i = 0; i < arr.length; i++) {
     if (column) {
       ctx.fillText(arr[i], x, yCoord);
-      yCoord += delta;
+      yCoord += UPPER_TEXT_Y_GAP;
     } else {
-      ctx.textBaseline = 'bottom';
       ctx.fillText(arr[i], xCoord, y);
-      xCoord += delta;
+      xCoord += NAMES_DELTA_X;
     }
   }
 };
 
-// Возвращает высоты столбцов гистаграммы
-var getColumnHeights = function (timesArray) {
+// Возвращает максимальное время
+var getMaxTime = function (timesArray) {
   var maxTime = timesArray[0];
-  var columnHeight = [];
   for (var i = 1; i < timesArray.length; i++) {
     if (maxTime < timesArray[i]) {
       maxTime = timesArray[i];
     }
   }
-  for (var j = 0; j < timesArray.length; j++) {
-    columnHeight[j] = Math.round(MAX_COLUMN_HEIGHT * timesArray[j] / maxTime);
+  return maxTime;
+};
+
+
+// Возвращает высоты столбцов гистаграммы
+var getColumnHeights = function (timesArray) {
+  var columnHeight = [];
+  for (var i = 0; i < timesArray.length; i++) {
+    columnHeight[i] = Math.round(MAX_COLUMN_HEIGHT * timesArray[i] / getMaxTime(timesArray));
   }
-  return (columnHeight);
+  return columnHeight;
+};
+
+// Получение случайного цвета
+var getRandomColorSaturation = function () {
+  return (Math.random().toFixed(1));
 };
 
 // Построение столбцов гистограммы
 var buildColumns = function (ctx, columnHeightsArray, timesArray, namesArray) {
   var startX = CLOUD_ORIGIN_X + COLUMN_ORIGIN_OFFSET_X;
   var startY = CLOUD_ORIGIN_Y + COLUMN_ORIGIN_OFFSET_Y;
-  var colorSaturation = 1;
 
-  ctx.fillStyle = 'rgba(0, 0, 255, ' + colorSaturation + ')';
   for (var i = 0; i < columnHeightsArray.length; i++) {
-    colorSaturation = Math.random().toFixed(1);
-    ctx.fillStyle = 'rgba(0, 0, 255, ' + colorSaturation + ')';
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomColorSaturation() + ')';
     if (namesArray[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
@@ -88,10 +95,10 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#000';
 
   // Отрисовка верхнего текста
-  fillTextArr(ctx, titleTextArray, CLOUD_ORIGIN_X + UPPER_TEXT_OFFSET, CLOUD_ORIGIN_Y + UPPER_TEXT_OFFSET, UPPER_TEXT_Y_GAP, true);
+  renderTaxt(ctx, titleTextArray, CLOUD_ORIGIN_X + UPPER_TEXT_OFFSET, CLOUD_ORIGIN_Y + UPPER_TEXT_OFFSET, true);
 
-  // // Отрисовка имен
-  fillTextArr(ctx, names, CLOUD_ORIGIN_X + NAMES_OFFSET_X, CLOUD_ORIGIN_Y + CLOUD_HEIGHT + NAMES_OFFSET_Y, NAMES_DELTA_X, false);
+  // Отрисовка имен
+  renderTaxt(ctx, names, CLOUD_ORIGIN_X + NAMES_OFFSET_X, CLOUD_ORIGIN_Y + CLOUD_HEIGHT + NAMES_OFFSET_Y, false);
   getColumnHeights(times);
   buildColumns(ctx, getColumnHeights(times), times, names);
 };
